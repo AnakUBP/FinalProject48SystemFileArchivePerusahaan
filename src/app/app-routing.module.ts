2dfb94c79e92f1f07e18ec0f67d085d081b87102
AveralAS <if23.averalshafier@mhs.ubpkarawang.ac.id>
2025-05-12 18:02:03 +0700

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'loading-page',
+    loadChildren: () => import('./loading-page/loading-page.module').then( m => m.LoadingPagePageModule)
+  },
+  {
+    path: 'forgot-password',
+    loadChildren: () => import('./forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
+  },
+  {
+    path: 'home-menu',
+    loadChildren: () => import('./home-menu/home-menu.module').then( m => m.HomeMenuPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/loading-page/loading-page.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-page',
+  templateUrl: './loading-page.page.html',
+  styleUrls: ['./loading-page.page.scss'],
+  standalone: false,
+})
+export class LoadingPagePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
