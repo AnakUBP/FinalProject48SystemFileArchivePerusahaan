ec0a13b98551a4f73e4338204e9db206d9b848df
farandgh <farandgemma0302@gmail.com>
2025-05-31 19:41:13 +0700

    JeniscutiSeeder.php

--- website/database/seeders/JeniscutiSeeder.php
+++ website/database/seeders/JeniscutiSeeder.php
@@ -9,9 +9,27 @@ class JenisCutiSeeder extends Seeder
 {
     public function run(): void
     {
+        // Ambil id template yang sudah ada
+        $template = DB::table('templates')->first();
+
+        if (!$template) {
+            echo "Seeder JenisCuti dilewati karena tidak ada data di tabel templates.\n";
+            return;
+        }
+
         DB::table('jenis_cuti')->insert([
-            ['nama_cuti' => 'Cuti Tahunan', 'kode_cuti' => 'CT-01', 'keterangan' => 'Cuti tahunan biasa'],
-            ['nama_cuti' => 'Cuti Sakit', 'kode_cuti' => 'CS-02', 'keterangan' => 'Cuti karena sakit'],
+            [
+                'nama_cuti' => 'Cuti Tahunan',
+                'kode_cuti' => 'CT-01',
+                'keterangan' => 'Cuti tahunan biasa',
+                'templates_id' => $template->id,
+            ],
+            [
+                'nama_cuti' => 'Cuti Sakit',
+                'kode_cuti' => 'CS-02',
+                'keterangan' => 'Cuti karena sakit',
+                'templates_id' => $template->id,
+            ],
         ]);
     }
 }
