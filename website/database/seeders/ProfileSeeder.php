362d119f4998f6be2af3497cb02e1e8d04305ac2
farandgh <farandgemma0302@gmail.com>
2025-05-31 19:59:42 +0700

    ProfileSeeder.php

--- website/database/seeders/ProfileSeeder.php
+++ website/database/seeders/ProfileSeeder.php
@@ -2,18 +2,23 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Seeder;
 use App\Models\Profile;
 use App\Models\User;
-use Illuminate\Database\Seeder;
 
 class ProfileSeeder extends Seeder
 {
     public function run(): void
     {
         $users = User::all();
+
         foreach ($users as $user) {
-            Profile::factory()->create([
+            Profile::create([
                 'user_id' => $user->id,
+                'alamat' => 'Jl. Contoh Alamat ' . $user->id,
+                'telepon' => '08' . rand(1000000000, 9999999999),
+                'tanggal_lahir' => now()->subYears(rand(20, 35))->subDays(rand(0, 365)),
+                'foto' => 'images/profiles/default.png',
             ]);
         }
     }
