ac3fba8e88c497f5c20696bb6fc7655368f37de5
farandgh <farandgemma0302@gmail.com>
2025-05-31 20:00:37 +0700

    TemplateSeeder.php

--- website/database/seeders/TemplateSeeder.php
+++ website/database/seeders/TemplateSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
 
 class TemplateSeeder extends Seeder
 {
@@ -13,8 +14,10 @@ class TemplateSeeder extends Seeder
             [
                 'nama_template' => 'Template Surat Cuti',
                 'kategori' => 'Cuti',
-                'file_path' => 'templates/surat_cuti.pdf'
-            ]
+                'file_path' => 'templates/surat_cuti.pdf',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
         ]);
     }
 }
